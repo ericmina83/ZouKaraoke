756190d7e02a8af9a8d34a8b007def4fe33db6fb
Jyun-An, Zou <ericmina83@gmail.com>
2022-01-29 02:36:50 +0800

    main window with play list

--- main.py
+++ main.py
@@ -1,99 +1,103 @@
-import re
-from cProfile import label
-from glob import glob
-from multiprocessing import Condition
-from xml.etree.ElementTree import tostring
-import threading
-from moviepy.editor import *
-from moviepy.audio.fx.volumex import *
-from os import walk
-import tkinter as tk
-import pygame
-
-movie_playing = False
-end_app = False
-searchText = None
-cv = Condition()
-path = "./Songs"
+from PyQt5.QtWidgets import QApplication, QWidget, QListView, QPushButton, QHBoxLayout, QVBoxLayout, QMessageBox, QFileDialog
+from ReadSongs import *
+from PyPlayer import PlayerWindow
+from PyQt5.QtCore import QStringListModel
+import sys
 
 
-def play_video():
-    global cv
-    global movie_playing
-    while True:
-        cv.acquire()
-        cv.wait_for(lambda: movie_playing)
-        cv.release()
+class MainWindow(QWidget):
+    def __init__(self):
+        super().__init__()
 
-        if end_app is True:
-            break
+        self.playerWindow = PlayerWindow()
 
-        clip = VideoFileClip('./Songs/YOASOBI「夜に駆ける」 Official Music Video.mp4')
-        clip.preview()
+        self.get_songs_path_from_input_dialog()
+        self.playList = self.songs.copy()  # songs will be played
+        self.selectedPlayListSong = None
 
-        clip.close()
+        # column 1, play list view
+        self.playListView = QListView()
+        self.playListView.setModel(QStringListModel())
+        self.update_play_list_view()
+        self.playListView.clicked.connect(self.play_list_view_selected)
 
-        print("song end")
+        # column 1, button bar, insert button
+        self.insertBtn = QPushButton("插播")
+        self.insertBtn.clicked.connect(self.on_play_list_insert_btn_clicked)
 
-        pygame.display.quit()
+        # column 1, button bar, remove button
+        self.removeBtn = QPushButton("移除")
+        self.removeBtn.clicked.connect(self.on_play_list_remove_btn_clicked)
 
-        movie_playing = False
+        # column 1, button bar
+        buttonBar = QHBoxLayout()
+        buttonBar.addWidget(self.insertBtn)
+        buttonBar.addWidget(self.removeBtn)
+
+        # column 1
+        column1 = QVBoxLayout()
+        column1.addWidget(self.playListView)
+        column1.addLayout(buttonBar)
+
+        # column 2
+        self.songsListView = QListView()
+        self.songsListView.setModel(QStringListModel())
+
+        # total horizontal box
+        hbox = QHBoxLayout()
+        hbox.addLayout(column1)
+
+        self.setLayout(hbox)
+
+    def print_songs(self):
+        print('---------')
+        for song in self.songs:
+            print(song.songName)
+
+    def print_play_list(self):
+        print('---------')
+        for song in self.playList:
+            print(song.songName)
+
+    def on_play_list_insert_btn_clicked(self):
+        if self.selectedPlayListSong is None:
+            QMessageBox.information(self, '錯誤', '請選擇歌曲後才能插播')
+        else:
+            self.playList.remove(self.selectedPlayListSong)
+            self.playList.insert(0, self.selectedPlayListSong)
+            self.selectedPlayListSong = None
+            self.update_play_list_view()
+
+    def on_play_list_remove_btn_clicked(self):
+        if self.selectedPlayListSong is None:
+            QMessageBox.information(self, '錯誤', '請選擇歌曲後才能移除')
+        else:
+            self.playList.remove(self.selectedPlayListSong)
+            self.selectedPlayListSong = None
+            self.update_play_list_view()
+
+    def play_list_view_selected(self, modelIndex):
+        self.selectedPlayListSong = self.playList[modelIndex.row()]
+        print(self.selectedPlayListSong.songName)
+
+    def update_play_list_view(self):
+        stringList = []
+
+        for song in self.playList:
+            stringList.append(song.songName)
+
+        self.playListView.model().setStringList(stringList)
+
+    def get_songs_path_from_input_dialog(self):
+        songsPath = QFileDialog.getExistingDirectory(
+            self, "請選擇歌庫路徑")
+
+        self.songs = list_all_songs(songsPath)
+        output_csv(self.songs)
 
 
-t = threading.Thread(target=play_video)
-t.start()
-
-
-def play_video_thread():
-    global movie_playing
-    if movie_playing == False:
-        movie_playing = True
-        cv.acquire()
-        cv.notify()
-        cv.release()
-
-
-window = tk.Tk()
-window.title('window')
-window.geometry('500x100')
-
-
-def list_all_files():
-    f = []
-    for (dirpath, dirnames, filenames) in walk(path):
-        f.extend(filenames)
-        break
-    print(f)
-    global searchText
-    print(searchText)
-
-
-def initialize(window):
-    playButton = tk.Button(window, text="play", command=play_video_thread)
-    playButton.pack()
-
-    listButton = tk.Button(window, text="list",
-                           command=list_all_files)
-    listButton.pack()
-
-    global searchText
-    searchText = tk.StringVar()
-    searchBar = tk.Entry(window, textvariable=searchText,
-                         validate='key', validatecommand=lambda: print("XX"))
-    searchBar.pack()
-
-
-initialize(window)
-
-window.mainloop()
-
-end_app = movie_playing = True
-
-cv.acquire()
-cv.notify()
-cv.release()
-
-t.join()
-
-print("app end")
-# pygame.quit()
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    mainwindow = MainWindow()
+    mainwindow.show()
+    sys.exit(app.exec_())
