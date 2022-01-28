c9f169ab6027b83cdb552e1d40e1b55b28486bc4
Jyun-An, Zou <ericmina83@gmail.com>
2022-01-29 07:41:52 +0800

    [feat] search system with singer and song's name

--- SearchListWidget.py
+++ SearchListWidget.py
@@ -1,4 +1,4 @@
-from PyQt5.QtWidgets import QWidget, QListView, QPushButton, QHBoxLayout, QVBoxLayout, QMessageBox, QFileDialog
+from PyQt5.QtWidgets import QWidget, QListView, QPushButton, QHBoxLayout, QVBoxLayout, QMessageBox, QLineEdit, QLabel
 from PyQt5.QtCore import QStringListModel
 from ReadSongs import *
 import sys
@@ -26,9 +26,52 @@ class SearchListWidget(QWidget):
 
         self.update_search_list_view()
 
+    def check_song_correct_or_not(self, song: Song):
+        if self.singerEdit.text() not in song.singer:
+            return False
+
+        if self.songNameEdit.text() not in song.name:
+            return False
+
+        return True
+
+    def on_edit_text_changed(self, str):
+        self.searchList = list(
+            filter(self.check_song_correct_or_not, self.mainWindow.songs))
+
+        self.update_search_list_view()
+
     def create_column2(self):
         # VBox
         vbox = QVBoxLayout()
+
+        # HBox singer
+        hbox1 = QHBoxLayout()
+        vbox.addLayout(hbox1)
+
+        # HBox singer, label
+        singerLabel = QLabel()
+        singerLabel.setText("歌手：")
+        hbox1.addWidget(singerLabel)
+
+        # HBox singer, line edit
+        self.singerEdit = QLineEdit()
+        self.singerEdit.textChanged.connect(self.on_edit_text_changed)
+        hbox1.addWidget(self.singerEdit)
+
+        # HBox song name
+        hbox2 = QHBoxLayout()
+        vbox.addLayout(hbox2)
+
+        # HBox song name, label
+        songNameLabel = QLabel()
+        songNameLabel.setText("歌名：")
+        hbox2.addWidget(songNameLabel)
+
+        # HBox song name, line edit
+        self.songNameEdit = QLineEdit()
+        self.songNameEdit.textChanged.connect(self.on_edit_text_changed)
+        hbox2.addWidget(self.songNameEdit)
 
         # search list view
         self.searchListView = QListView()
