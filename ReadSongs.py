8d911fcba40d4c75305d5a21b42490da7ef3dcfd
Jyun-An, Zou <ericmina83@gmail.com>
2022-01-29 02:35:37 +0800

    new method to get song's validation

--- ReadSongs.py
+++ ReadSongs.py
@@ -1,6 +1,15 @@
 import os
 import re
 
+idMatch = re.compile('^[a-j][1-8]\d{6}$')
+langMatch = re.compile("^((國|台|粵|日|喔|客|韓)語|兒歌|其他|原住民)$")
+
+
+class Singer():
+    def __init__(self, name, gender):
+        self.name = name
+        self.gender = gender
+
 
 class Song():
     def __init__(self, songPath, songId, songLang, singer, songName):
@@ -14,43 +23,48 @@ class Song():
         return self.songId + "," + self.songLang + "," + self.songName + "," + self.singer + "," + self.songPath
 
 
+def check_filename(basename, extension):
+
+    if extension != ".mp4":
+        print("%s is not mp4" % basename)
+        return False
+
+    strs = basename.split("_")
+
+    if len(strs) != 4:
+        print("%s format isn't correct" % basename)
+        return False
+
+    if idMatch.match(strs[0]) is None:
+        print("%s id is not ok" % basename)
+        return False
+
+    if langMatch.match(strs[1]) is None:
+        print("%s lang is not ok" % basename)
+        return False
+
+    return True
+
+
 def list_all_songs(path):
     path = os.path.abspath(path)
     print("Songs abspath: %s" % path)
-
-    idMatch = re.compile('^[a-j][1-8]\d{6}$')
-    langMatch = re.compile("^((國|台|粵|日|喔|客|韓)語|兒歌|其他|原住民)$")
 
     songs = []
 
     for (dirpath, dirnames, filenames) in os.walk(path):
 
         for filename in filenames:
-            basename = os.path.splitext(filename)[0]
-            extension = os.path.splitext(filename)[1]
-
             songpath = os.path.join(dirpath, filename)
             print("songpath is %s" % songpath)
 
-            if extension != ".mp4":
-                print("%s is not mp4" % filename)
+            basename = os.path.splitext(filename)[0]
+            extension = os.path.splitext(filename)[1]
+
+            if check_filename(basename, extension) is False:
                 continue
 
             strs = basename.split("_")
-
-            if len(strs) != 4:
-                print("%s format isn't correct" % filename)
-                continue
-
-            if idMatch.match(strs[0]):
-                print("%s id is ok" % filename)
-            else:
-                print("%s id is not ok" % filename)
-
-            if langMatch.match(strs[1]):
-                print("%s lang is ok" % filename)
-            else:
-                print("%s lang is not ok" % filename)
 
             songs.append(Song(songpath, strs[0], strs[1], strs[2], strs[3]))
 
